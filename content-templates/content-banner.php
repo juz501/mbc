78d19171cdf64faf7fe8358a8e3fbd32150f3ebb
Julian Chan <julian@Julians-MacBook-Pro.local>
2018-06-17 12:36:54 +1000

    tablet and laptop banner fixes

--- content-templates/content-banner.php
+++ content-templates/content-banner.php
@@ -1,40 +1,46 @@
 <?php
     $use_video = get_field( 'use_video' );
     $banner = get_field( 'banner_image' ); 
+    $banner_laptop = get_field( 'banner_image_laptop' );
+    if ( ! $banner_laptop ) : 
+      $banner_laptop = $banner;
+    endif; 
     $banner_tablet = get_field( 'banner_image_tablet' );
     if ( ! $banner_tablet ) : 
-      $banner_tablet = $banner;
+      $banner_tablet = $banner_laptop;
     endif; 
     $banner_mobile = get_field( 'banner_image_mobile' );
     if ( ! $banner_mobile ) : 
-      $banner_mobile = $banner;
+      $banner_mobile = $banner_tablet;
     endif; 
   ?>
 <section class="top-section">
   <style>
     .home .top-section {
-      height: 700px;
+      height: 630px;
     }
     .home .banner {
       background-image: url('<?php echo $banner_mobile["url"]; ?>');
       background-size: contain;
     }
     @media screen and (min-width: 600px) {
-      .home .top-section {
-        height: 700px;
-      }
       .home .banner {
         background-image: url('<?php echo $banner_tablet["url"]; ?>');
         background-size: contain;
       }
     }
-    @media screen and (min-width: 1280px) {
+    @media screen and (min-width: 1024px) {
       .home .top-section {
         height: 700px;
       }
       .home .banner {
-        background-image: url('<?php echo $banner["url"]; ?>');
+        background-image: url('<?php echo $banner_laptop["url"]; ?>');
         background-size: cover;
+      }
+    }
+    @media screen and (min-width: 1240px) {
+      .home .banner {
+        background-image: url('<?php echo $banner["url"]; ?>');
       }
     }
   </style>
