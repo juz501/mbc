7d00691f98f9e9b15a0b916a10ed617e0c516a73
Julian Chan <julian@Julians-MacBook-Pro.local>
2018-10-06 13:36:49 +1000

    add composer

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
-get_header(); 
+/**
+ * Index file which runs the content loop and includes header and footer
+ */
+
+get_header();
 get_template_part( 'content-templates/content', 'loop' );
 get_footer();
